fb1953b7f52aab967b6709c1589bff3020716b3c
Thiago Adriano <thiago.s.adriano@gmail.com>
2017-08-28 09:11:52 -0300

    feito a validação dos campos

--- src/ts/controller/register.controller.ts
+++ src/ts/controller/register.controller.ts
@@ -20,14 +20,54 @@ namespace APPWZ {
          * @param isValid {boolean} - recebe do formulário se o mesmo está válido
          */
         function sendForm(isValid){
-            if(isValid){
+            if(!isValid){
+                MsgInfos.danger("Preencha os campos com a borda vermelha");
+                return;
+            }
+
+            if(validateCampos()){
                 RS.Salvar(vm.Model).then(()=>{
                     PM.newModel();
                     $state.go("platforms")
                 });
-            }else{
-                MsgInfos.danger("Preencha os campos com a borda vermelha");
             }
+        }
+
+        /**
+         * Valida se os dados recebidos conferem com o padrão estabelecido
+         */
+        function validateCampos(){
+            let cpfPattern = /\d{3}\.\d{3}\.\d{3}-\d{2}/,
+                dataPattern = /\d{2}\/\d{2}\/\d{4}/,
+                emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+                telPattern = /\(\d{2}\)\s\d{4,5}-\d{4}/,
+                invalid = 0;
+            
+            if(!cpfPattern.test(vm.Model.CPF)){
+                MsgInfos.danger("O CPF não está no padrão correto. Ex: 123.456.789-10");
+                invalid++;
+            }
+
+            if(!dataPattern.test(vm.Model.Nascimento)){
+                MsgInfos.danger("A data de Nascimento não está no padrão correto. Ex: 12/10/1980");
+                invalid++;                
+            }
+
+            if(!emailPattern.test(vm.Model.Email)){
+                MsgInfos.danger("O Email não está no padrão correto. Ex: fulano@beltrano.com");
+                invalid++;                
+            }
+
+            if(!telPattern.test(vm.Model.Telefone)){
+                MsgInfos.danger("O Telefone não está no padrão correto. Ex: (21) 23456-9874");
+                invalid++;                
+            }
+
+            if(invalid > 0){
+                return false;
+            }
+
+            return true;
         }
 
         /**
